094244c3acd1c422462c2e6cb98fef719d83c233
Stanya Palmaro <spalmaro@41.student.fr>
2016-12-08 00:29:11 +0100

    new ftprintf file

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -1,8 +1,17 @@
 #include "../includes/ft_printf.h"
 
-int		ft_printf(const char * restrict format, ...)
+int		ft_printf(const char *format, ...)
 {
 	va_list		lst;
+	char		*nxt_conv;
 
 	va_start(lst, format);
+	nxt_conv = ft_strchr(lst, '%');
+	if (*format == '\0')
+		return (0);
+	if (!nxt_conv)
+	{
+		ft_putstr(format);
+		return (ft_strlen(format));
+	}
 }
