b777ca4e26d6079b770311bb089b0f4db8adae37
Stanya Palmaro <spalmaro@41.student.fr>
2016-12-07 22:42:55 +0100

    update

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -18,5 +18,6 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdarg.h>
 
 #endif
---
+++ src/ft_printf.c
@@ -0,0 +1,8 @@
+#include "../includes/ft_printf.h"
+
+int		ft_printf(const char * restrict format, ...)
+{
+	va_list		lst;
+
+	va_start(lst, format);
+}
