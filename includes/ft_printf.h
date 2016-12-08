accad9df24f7f8a989bd2b7cd935de1a8fb1139a
Stanya PALMARO <spalmaro@e3r8p16.42.fr>
2016-12-08 18:04:52 +0100

    update

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: qduperon <qduperon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/07 11:26:14 by qduperon          #+#    #+#             */
-/*   Updated: 2016/12/07 17:13:23 by spalmaro         ###   ########.fr       */
+/*   Updated: 2016/12/08 16:49:55 by spalmaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define FT_PRINTF_H
 
 # define CONVERSION sSpdDioOuUxXcC
-# define FLAGS %#0-+hljz
+# define FLAGS #0-+hljz
 
 # include <unistd.h>
 # include <stdlib.h>
--- src/ft_printf.c
+++ src/ft_printf.c
@@ -1,5 +1,18 @@
 #include "../includes/ft_printf.h"
 
+int		ft_parse(char *next_conv)
+{
+	int i;
+
+	i = 1;
+	if (next_conv[i] == '%')
+		ft_putchar('%');
+	while (next_conv[i])
+	{
+		
+	}
+}
+
 int		ft_printf(const char *format, ...)
 {
 	va_list		lst;
@@ -14,4 +27,7 @@ int		ft_printf(const char *format, ...)
 		ft_putstr(format);
 		return (ft_strlen(format));
 	}
+	if (!(ft_parse(next_conv))
+		return (-1); //idk what its supposed to send
+
 }
